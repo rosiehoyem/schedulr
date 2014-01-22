f55adc8573f534eb0eb701e44953971ad0703218
Rosie Hoyem <rosiehoyem@gmail.com>
2014-01-21 22:32:43 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Schedulr::Application.config.secret_key_base = '23246771f63153d5140a685eb9095ae50595e2acf6ed1dadfad0ee8289e67d57c910861d852b46018c2565485ecab967f51c4c8dc9bd545105f6c26bc04ca4b6'
